7f8109c3ca0fb50fddfafd9a11ee7326e6b9df20
Avvami <dima.valbu.01@gmail.com>
2024-09-03 22:16:00 +0700

    Viewer for profiles

--- app/src/main/java/com/personal/tmdb/detail/presentation/image/ImageViewerScreen.kt
+++ app/src/main/java/com/personal/tmdb/detail/presentation/image/ImageViewerScreen.kt
@@ -22,19 +22,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.KeyboardArrowLeft
 import androidx.compose.material.icons.rounded.Share
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -46,9 +50,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
@@ -68,6 +74,7 @@ import com.personal.tmdb.core.util.findActivity
 import com.personal.tmdb.core.util.shareText
 import com.personal.tmdb.ui.theme.backgroundLight
 import com.personal.tmdb.ui.theme.scrimLight
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
@@ -76,8 +83,10 @@ fun ImageViewerScreen(
     imageViewerViewModel: ImageViewerViewModel = hiltViewModel()
 ) {
     val horizontalPagerState = rememberPagerState(
-        pageCount = { 50 }
+        pageCount = { imageViewerViewModel.imagesState.images?.size ?: 0 },
+        initialPage = imageViewerViewModel.initialPage
     )
+    val scope = rememberCoroutineScope()
     val activity = LocalContext.current.findActivity() as MainActivity
     val windowInsetsController = WindowCompat.getInsetsController(activity.window, activity.window.decorView)
     BackHandler {
@@ -98,83 +107,86 @@ fun ImageViewerScreen(
             .background(scrimLight)
             .navigationBarsPadding()
     ) {
-        AnimatedContent(
-            targetState = imageViewerViewModel.showGridView,
-            label = "Image viewer animation",
-            transitionSpec = {
-                scaleIn(initialScale = .7f) + fadeIn() togetherWith scaleOut(targetScale = .7f) + fadeOut()
-            }
-        ) { targetState ->
-            if (targetState) {
-                LazyVerticalGrid(
-                    modifier = Modifier
-                        .statusBarsPadding()
-                        .padding(top = 64.dp),/*TODO: Update the hardcoded value when Material 1.3.0 release*/
-                    columns = GridCells.Adaptive(80.dp),
-                    contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(2.dp),
-                    verticalArrangement = Arrangement.spacedBy(2.dp)
-                ) {
-                    items(
-                        count = 50
-                    ) {
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .aspectRatio(1f)
-                                .clip(MaterialTheme.shapes.extraSmall)
-                                .clickable {
-                                    imageViewerViewModel.imageViewerUiEvent(ImageViewerUiEvent.ChangeShowGridView)
-                                },
-                            model = C.TMDB_IMAGES_BASE_URL + C.PROFILE_W185 + "/ghNt7Sqqve1SpLUoRUxAoZHLkJW.jpg",
-                            contentDescription = "Image",
-                            placeholder = painterResource(id = R.drawable.placeholder),
-                            error = painterResource(id = R.drawable.placeholder),
-                            contentScale = ContentScale.Crop
-                        )
-                    }
+        imageViewerViewModel.imagesState.images?.let { images ->
+            AnimatedContent(
+                targetState = imageViewerViewModel.showGridView,
+                label = "Image viewer animation",
+                transitionSpec = {
+                    scaleIn(initialScale = .7f) + fadeIn() togetherWith scaleOut(targetScale = .7f) + fadeOut()
                 }
-            } else {
-                CompositionLocalProvider(
-                    LocalOverscrollConfiguration provides null
-                ) {
-                    HorizontalPager(
+            ) { targetState ->
+                if (targetState) {
+                    LazyVerticalGrid(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .align(Alignment.Center),
-                        state = horizontalPagerState,
-                        beyondViewportPageCount = 2,
-                        pageSize = PageSize.Fill
-                    ) { page ->
-                        AsyncImage(
+                            .statusBarsPadding()
+                            .padding(top = 64.dp),/*TODO: Update the hardcoded value when Material 1.3.0 release*/
+                        columns = GridCells.Adaptive(80.dp),
+                        contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp),
+                        horizontalArrangement = Arrangement.spacedBy(2.dp),
+                        verticalArrangement = Arrangement.spacedBy(2.dp)
+                    ) {
+                        itemsIndexed(
+                            items = images
+                        ) { index, image ->
+                            AsyncImage(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .aspectRatio(1f)
+                                    .clip(MaterialTheme.shapes.extraSmall)
+                                    .clickable {
+                                        imageViewerViewModel.imageViewerUiEvent(ImageViewerUiEvent.ChangeShowGridView)
+                                        scope.launch {
+                                            horizontalPagerState.scrollToPage(index)
+                                        }
+                                    },
+                                model = C.TMDB_IMAGES_BASE_URL + C.PROFILE_W185 + image?.filePath,
+                                contentDescription = "Image",
+                                placeholder = painterResource(id = R.drawable.placeholder),
+                                error = painterResource(id = R.drawable.placeholder),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    }
+                } else {
+                    CompositionLocalProvider(
+                        LocalOverscrollConfiguration provides null
+                    ) {
+                        HorizontalPager(
                             modifier = Modifier
                                 .fillMaxSize()
-                                .graphicsLayer {
-                                    val endOffset = (
-                                            (horizontalPagerState.currentPage - page) + horizontalPagerState
-                                                .currentPageOffsetFraction
-                                            ).coerceAtMost(0f)
-                                    alpha = 1f + endOffset
-                                    val scale = 1f + (endOffset * .2f)
-                                    scaleX = scale
-                                    scaleY = scale
-                                }
-                                .clickable(
-                                    interactionSource = remember { MutableInteractionSource() },
-                                    indication = null
-                                ) {
-                                    imageViewerViewModel.imageViewerUiEvent(ImageViewerUiEvent.ChangeHideUi)
-                                    if (imageViewerViewModel.hideUi) {
-                                        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-                                    } else {
-                                        windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+                                .align(Alignment.Center),
+                            state = horizontalPagerState,
+                            beyondViewportPageCount = 2,
+                            pageSize = PageSize.Fill
+                        ) { page ->
+                            AsyncImage(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .graphicsLayer {
+                                        val endOffset = (
+                                                (horizontalPagerState.currentPage - page) + horizontalPagerState
+                                                    .currentPageOffsetFraction
+                                                ).coerceAtMost(0f)
+                                        alpha = 1f + endOffset
+                                        val scale = 1f + (endOffset * .2f)
+                                        scaleX = scale
+                                        scaleY = scale
                                     }
-                                },
-                            model = C.TMDB_IMAGES_BASE_URL + C.ORIGINAL + "/ghNt7Sqqve1SpLUoRUxAoZHLkJW.jpg",
-                            contentDescription = "Image",
-                            placeholder = painterResource(id = R.drawable.placeholder),
-                            error = painterResource(id = R.drawable.placeholder)
-                        )
+                                    .clickable(
+                                        interactionSource = remember { MutableInteractionSource() },
+                                        indication = null
+                                    ) {
+                                        imageViewerViewModel.imageViewerUiEvent(ImageViewerUiEvent.ChangeHideUi)
+                                        if (imageViewerViewModel.hideUi) {
+                                            windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+                                        } else {
+                                            windowInsetsController.show(WindowInsetsCompat.Type.systemBars())
+                                        }
+                                    },
+                                model = C.TMDB_IMAGES_BASE_URL + C.ORIGINAL + images[page]?.filePath,
+                                contentDescription = "Image"
+                            )
+                        }
                     }
                 }
             }
@@ -245,13 +257,12 @@ fun ImageViewerScreen(
                     )
                 )
                 androidx.compose.animation.AnimatedVisibility(
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally),
-                    visible = !imageViewerViewModel.showGridView,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    visible = !imageViewerViewModel.showGridView && horizontalPagerState.pageCount != 0,
                     enter = slideInVertically() + fadeIn(animationSpec = tween(delayMillis = 50)),
                     exit = slideOutVertically() + fadeOut(animationSpec = tween(durationMillis = 250))
                 ) {
-                    Text(
+                    Row(
                         modifier = Modifier
                             .clip(CircleShape)
                             .background(scrimLight.copy(.5f))
@@ -261,15 +272,27 @@ fun ImageViewerScreen(
                             ) {
                                 imageViewerViewModel.imageViewerUiEvent(ImageViewerUiEvent.ChangeShowGridView)
                             }
-                            .padding(horizontal = 6.dp, vertical = 2.dp),
-                        text = stringResource(
-                            id = R.string.image_viewer_count,
-                            (horizontalPagerState.currentPage + 1),
-                            horizontalPagerState.pageCount
-                        ),
-                        style = MaterialTheme.typography.labelLarge,
-                        color = backgroundLight
-                    )
+                            .padding(start = 6.dp, top = 2.dp, end = 4.dp, bottom = 2.dp),
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        Text(
+                            text = stringResource(
+                                id = R.string.image_viewer_count,
+                                (horizontalPagerState.currentPage + 1),
+                                horizontalPagerState.pageCount
+                            ),
+                            style = MaterialTheme.typography.labelLarge,
+                            color = backgroundLight
+                        )
+                        Icon(
+                            modifier = Modifier
+                                .size(18.dp)
+                                .rotate(-90f),
+                            imageVector = Icons.AutoMirrored.Rounded.KeyboardArrowLeft,
+                            contentDescription = "Dropdown",
+                            tint = backgroundLight
+                        )
+                    }
                 }
             }
         }
